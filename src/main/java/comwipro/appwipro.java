43c01d960b59592327fcd6df7b70d72955794159
rajaa153 <rajaa153@gmail.com>
2022-01-14 17:22:28 +0530

    hai

---
+++ src/main/java/comwipro/appwipro.java
@@ -0,0 +1,21 @@
+package comwipro;
+
+public class appwipro {
+
+
+
+		  public static void main(String[] args) {
+		    
+		    System.out.println("Enter two numbers");
+		    int first = 10;
+		    int second = 44;
+		    
+		    System.out.println(first + " " + second);
+
+		    // add two numbers
+		    int sum = first + second;
+		    System.out.println("The sum is: " + sum);
+		  }
+		}
+	
+	
